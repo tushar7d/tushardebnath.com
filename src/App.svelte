273f01aa95f3b0643b894236c2b8f76b9e0feb47
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2020-09-27 02:33:53 +0530

    changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,17 @@
 <script>
+  import Right from "./Right.svelte";
+  import Left from "./Left.svelte";
 </script>
 
 <style>
-  h1 {
-    width: 50%;
-    font-size: 2em;
-  }
   main {
     max-width: 1280px;
     margin: 0 auto 0 auto;
     display: flex;
   }
-  .left {
-    flex: 1 0 auto;
-    background-color: antiquewhite;
-    width: 50%;
-    padding: 12px;
-  }
-  .right {
-    flex: 1 0 auto;
-    background-color: aqua;
-    width: 50%;
-    padding: 12px;
-  }
 </style>
 
 <main>
-  <div class="left">
-    <h1>Hello, I’m Tushar Debnath.</h1>
-    <p>
-      I’m a Product designer from New Delhi, India. I am currently working at
-      Zomato as a Senior Product Designer .
-    </p>
-  </div>
-  <div class="right">right</div>
+  <Left />
+  <Right />
 </main>
---
+++ src/Right.svelte
@@ -0,0 +1,14 @@
+
+	
+
+			
+<style>
+    .right {
+    flex: 1 0 auto;
+    background-color: aqua;
+    width: 50%;
+    padding: 12px;
+  }
+			
+</style>
+<div class="right">right</div>
