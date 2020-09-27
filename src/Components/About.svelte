9f9b90a6b46b6321aa566cf278d8c98acbbdc38f
Tushar Debnath <txdebnath@gmail.com>
2020-09-27 17:20:39 +0530

    changes

---
+++ src/Components/About.svelte
@@ -0,0 +1,28 @@
+<style>
+  section {
+    flex: 1;
+  }
+
+  .padded_box {
+    padding: 12px;
+  }
+
+  h1 {
+    font-size: 48px;
+  }
+  p {
+    font-size: 1.2em;
+    line-height: 1.5;
+    color: grey;
+  }
+</style>
+
+<section>
+  <div class="padded_box">
+    <h1>Hello,<br /> I’m Tushar Debnath.</h1>
+    <p>
+      I’m a Product designer from New Delhi, India. I am currently working at
+      Zomato as a Senior Product Designer .
+    </p>
+  </div>
+</section>
