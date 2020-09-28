e57fbf3ab661e46308eef268ad3c0b45843c46ae
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2020-09-29 02:28:39 +0530

    added placeholder for Content section

--- src/Components/Content.svelte
+++ src/Components/Content.svelte
@@ -3,133 +3,56 @@
         display: flex;
         flex-direction: column;
         justify-content: space-between;
-        padding: 24px;
-       height: calc(100% - 200px);
+        padding: 80px;
+       height: calc(100% - 120px);
     }
     h1 {
-        font-size: 44px;
+        font-size: 52px;
         line-height: 1.2;
         font-weight: 600;
         margin: 0px;
-        margin-bottom: 12px;
+        margin-bottom: 28px;
     }
     p {
-        font-size: 14px;
+        font-size: 18px;
         line-height: 1.5;
         margin: 0px;
     }
     .quick_link_container{
 
         display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
+        
+        align-items: center;
     }
     .image_placeholder{
-        width: 80px;
-        height:80px;
+        width: 60px;
+        height:60px;
         border-radius: 160px;
         background-color: white;
 
+
+    }
+    .link{
+        margin-left: 12px;
     }
 
-    /* Extra small devices (phones, 600px and down) */
-    @media only screen and (max-width: 600px) {
-        section {
-           
-            padding: 24px;
-        }
-
-        p {
-            font-size: 14px;
-            line-height: 1.5;
-            margin: 0px;
-        }
-    }
-
-    /* Small devices (portrait tablets and large phones, 600px and up) */
-    @media only screen and (min-width: 600px) {
-        section {
-          
-            padding: 24px;
-        }
-
-        p {
-            font-size: 14px;
-            line-height: 1.5;
-            margin: 0px;
-        }
-    }
-
-    /* Medium devices (landscape tablets, 768px and up) */
-    @media only screen and (min-width: 768px) {
-        section {
-            
-            padding: 48px;
-        }
-        h1 {
-            font-size: 44px;
-            line-height: 1.2;
-            margin: 0px;
-            margin-bottom: 12px;
-        }
-        p {
-            font-size: 14px;
-            line-height: 1.5;
-            margin: 0px;
-        }
-    }
-
-    /* Large devices (laptops/desktops, 992px and up) */
-    @media only screen and (min-width: 992px) {
-        section {
-         
-            padding: 48px;
-            padding-top: 80px;
-        }
-        h1 {
-            font-size: 52px;
-            line-height: 1.2;
-            margin: 0px;
-            margin-bottom: 12px;
-        }
-        p {
-            font-size: 14px;
-            line-height: 1.5;
-            margin: 0px;
-        }
-    }
-
-    /* Extra large devices (large laptops and desktops, 1200px and up) */
-    @media only screen and (min-width: 1200px) {
-        section {
-            
-            padding: 48px;
-            padding-top: 80px;
-        }
-        h1 {
-            font-size: 52px;
-            line-height: 1.2;
-            margin: 0px;
-            margin-bottom: 12px;
-        }
-        p {
-            font-size: 14px;
-            line-height: 1.5;
-            margin: 0px;
-        }
-    }
 </style>
 
 <section>
     <div>
         <h1>Hello, I’m Tushar Debnath</h1>
         <p class="paragraph">
-            I am Tushar Debnath a Product designer & Design technologist living
+            A Product designer & Design technologist living
             in New Delhi. Currently I am building consumer products and internal
             design tools at Zomato.
         </p>
     </div>
     <div class="quick_link_container">
-        <div class="image_placeholder"></div>
+        <div class="image_placeholder" />
+        <div class="link">LinkedIn</div>
+        <div class="link">Figma</div>
+        <div class="link">Dribbble</div>
+        <div class="link">Github</div>
+
     </div>
 </section>
